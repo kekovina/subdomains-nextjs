c973af480ca28463c4cef260bf0e60aaa63dc264
kekovina <banderes94@gmail.com>
2021-12-29 21:26:44 +0300

    t

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const next = require('next')
 const vhost = require('vhost')
 
 const port = process.env.PORT || 3000
-const dev = process.env.NODE_ENV !== 'production'
+const dev = process.env.NODE_ENV === 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 const domain = dev ? 'lvh.me' : 'localhost'
@@ -17,16 +17,17 @@ app.prepare().then(() => {
 
   servers.map((server, index) => {
     server.get('/', (req, res) => {
-      console.log(`1212/${subdomains[index].slice(0, -1)}`)
+      console.log(`handle 1 /${subdomains[index].slice(0, -1)}`)
       return app.render(req, res, `/${subdomains[index].slice(0, -1)}`, req.query)
     })
   
     server.get('/*', (req, res) => {
-      console.log(`/${subdomains[index].slice(0, -1)}${req.path}`)
+      console.log(`handle 2 /${subdomains[index].slice(0, -1)}${req.path}`)
       return app.render(req, res, `/${subdomains[index].slice(0, -1)}${req.path}`, req.query)
     })
   
     server.all('*', (req, res) => {
+      console.log('handle 3')
       return handle(req, res)
     })
   })
@@ -36,14 +37,17 @@ app.prepare().then(() => {
   })
 
   mainServer.get('/', (req, res) => {
+    console.log('handle main 1')
     return app.render(req, res, `/main`, req.query)
   })
 
   mainServer.get('/*', (req, res) => {
+    console.log('handle main 2')
     return app.render(req, res, `/main${req.path}`, req.query)
   })
 
   mainServer.all('*', (req, res) => {
+    console.log('handle main 3')
     return handle(req, res)
   })
 
