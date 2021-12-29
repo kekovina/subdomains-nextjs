8cf13936924e902374258420734e1fe8f4fc1550
kekovina <banderes94@gmail.com>
2021-12-29 18:32:03 +0300

    'init'

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const next = require('next')
+const vhost = require('vhost')
+
+const port = process.env.PORT || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+const domain = 'lvh.me'
+
+app.prepare().then(() => {
+
+  const subdomains = ['admin.', 'member.']
+
+  const servers = subdomains.map(servername => express())
+  const mainServer = express()
+
+  servers.map((server, index) => {
+    server.get('/', (req, res) => {
+      console.log(`1212/${subdomains[index].slice(0, -1)}`)
+      return app.render(req, res, `/${subdomains[index].slice(0, -1)}`, req.query)
+    })
+  
+    server.get('/*', (req, res) => {
+      console.log(`/${subdomains[index].slice(0, -1)}${req.path}`)
+      return app.render(req, res, `/${subdomains[index].slice(0, -1)}${req.path}`, req.query)
+    })
+  
+    server.all('*', (req, res) => {
+      return handle(req, res)
+    })
+  })
+
+  servers.map((server, index) =>{
+    mainServer.use(vhost(`${subdomains[index]}${domain}`, server))
+  })
+
+  mainServer.get('/', (req, res) => {
+    return app.render(req, res, `/main`, req.query)
+  })
+
+  mainServer.get('/*', (req, res) => {
+    return app.render(req, res, `/main${req.path}`, req.query)
+  })
+
+  mainServer.all('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  mainServer.listen(port, (err) => {
+    if (err) throw err
+
+    console.log(`> Ready on http://${domain}:${port}`)
+  })
+})
